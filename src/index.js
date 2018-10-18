7c99d3f3fa17c6d53320ddf1ae2840835e73feb7
ahdeshpande <deshpande.abhiraj@gmail.com>
2018-10-17 22:57:57 -0400

    feat: create a ContactList component

--- src/index.js
+++ src/index.js
@@ -4,23 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-class ContactList extends React.Component {
-    render() {
-        const people = [
-            {name: 'Tyler'},
-            {name: 'Karen'},
-            {name: 'Richard'},
-        ];
-
-        return <ol>
-            {people.map((person) => (
-                <li key={person.name}>{person.name}</li>
-            ))}
-        </ol>
-    }
-}
-
-ReactDOM.render(<ContactList/>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
